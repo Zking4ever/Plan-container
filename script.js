f19ab59a0bd522d4daa555ca0e5e57366bda3964
Astawus zking4ever <yanosastawus@gmail.com>
2025-03-10 02:16:02 -0700

    made the button resposive for assigning date

---
+++ script.js
@@ -0,0 +1,5 @@
+var focus_date = document.getElementById("focus_date_selector");
+var focus_task = document.getElementById("focus_task_assign");
+function add_task(){
+    focus_date.style.display = "flex";
+}
